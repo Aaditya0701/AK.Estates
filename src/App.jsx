46f2b970603d0d788db2360b047d3673b39706d7
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-08-03 14:47:30 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1  className='text-red-500'>Aaditya</h1>
+      <h1  className='text-red-500'>Aaditya Kamlaskar</h1>
     </div>
   )
 }
