09bdaff46b9d0eab5c188a9f50a91b62e64bd0e0
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-08-04 12:22:47 +0530

    Created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 export default function App() {
   return (
-    <div>
-      <h1  className='text-red-500'>Aaditya Kamlaskar</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
