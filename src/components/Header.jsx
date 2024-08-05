6c0298b942b72bf548275c515f3cf9f5dabe7ff1
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-08-05 14:22:50 +0530

    Herder Component Created

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FaSearch } from "react-icons/fa";
+import { RiLoginBoxLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+
+
+export default function Header() {
+    return (
+        <header className='bg-slate-200 shadow-md'>
+            <div className='flex justify-between items-center max-w-7xl mx-auto p-3'>
+                <Link to='/'>
+                    <h1 className='font-bold text-sm sm:text-3xl flex flex-wrap'>
+                        <spam className='text-slate-500'>AK</spam>
+                        <spam className='text-slate-700'>Estate</spam>
+                    </h1>
+                </Link>
+                <form className='bg-slate-100 p-2 rounded-3xl flex items-center'>
+                    <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+                    <FaSearch className='text-slate-600' />
+                </form>
+                <ul className='flex items-center gap-5'>
+                    <Link to='/'>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                    </Link>
+                    <Link to='/about'>
+                        <li className='hidden sm:inline text-slate-700 hover:underline'>About Us</li>
+                    </Link>
+                    <Link to='/signin'>
+                        <li className='sm:flex text-slate-700 hover:bg-slate-300 p-2'><RiLoginBoxLine /></li>
+                    </Link>
+                </ul>
+            </div>
+        </header>
+    )
+}
