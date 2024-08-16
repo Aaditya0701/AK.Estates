1eb2f4c6bf247305169c8c58ad99049293d999f0
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-08-16 14:03:03 +0530

    Create a test api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = mongoose.Schema({
         required: true,
         unique: true
     },
-    userName:{
+    password:{
         type: String,
         required: true,
         unique: true
