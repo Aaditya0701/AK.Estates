5cc96ea08ccec42e3daac2efc128fe6926536f3e
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-08-17 11:33:57 +0530

    Created a middleware function to haldle error and a Function to throw custom errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { userName, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10); //encrypting the password
     const newUser = new User({ userName, email, password: hashedPassword });
@@ -10,7 +10,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json('user created successfully');
     } catch (error) { // if finds any duplicate value
-        res.status(500).json(error.message);
+        next(error);
     }
 
 };
