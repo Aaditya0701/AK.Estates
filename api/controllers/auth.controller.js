a368aa72467a77f68719e5ebcf8d4e9df8b7cd36
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-08-21 16:16:26 +0530

    Signin Page Sunctionality Completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,7 +19,7 @@ export const signup = async (req, res, next) => {
 };
 
 //signin page sunctionality
-export const signin = async (req, res, next) => {
+/* export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try {
         const validUser = await User.findOne({ email });
@@ -35,4 +35,24 @@ export const signin = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+} */
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User Not Found'));
+
+        const validPswd = bcryptjs.compareSync(password, validUser.password);
+        if (!validPswd) return next(errorHandler(404, 'Invalid Password'));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pswd, ...rest } = validUser._doc;
+
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json({ success: true, ...rest });
+    } catch (error) {
+        next(error);
+    }
+};
