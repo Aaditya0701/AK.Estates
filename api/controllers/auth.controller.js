6c8f63c6aa0f06a8ae030d1c2d8279ca0a2d5368
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-08-21 15:27:49 +0530

    SignIn page API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
+//signup page functionality
 export const signup = async (req, res, next) => {
     const { userName, email, password, confirmPassword } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10); //encrypting the password
@@ -14,3 +17,22 @@ export const signup = async (req, res, next) => {
     }
 
 };
+
+//signin page sunctionality
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User Not Found'));
+        const validPswd = bcryptjs.compareSync(password, validUser.password);
+        if (!validPswd) return next(errorHandler(404, 'Invalid Password'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const {password: pswd, ...rest} = validUser._doc //not to sent value of password in res.json(rest) 👇🏻
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest); // 👈🏻
+    } catch (error) {
+        next(error);
+    }
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
+//signup route
 router.post('/signup', signup);
+
+//sognin route
+router.post('/signin', signin)
 
 export default router;
