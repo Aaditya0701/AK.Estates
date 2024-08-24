6d3766497a536bdbded4bdcaa9e36e155f6ea448
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-08-24 18:57:03 +0530

    Added Login Imformation to header and made the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { FaSearch } from "react-icons/fa";
 import { RiLoginBoxLine } from "react-icons/ri";
 import { Link } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+    const { currentUser } = useSelector(state => state.user)
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-7xl mx-auto p-3'>
@@ -25,9 +26,21 @@ export default function Header() {
                     <Link to='/about'>
                         <li className='hidden sm:inline text-slate-700 hover:underline'>About Us</li>
                     </Link>
-                    <Link to='/signin'>
-                        <li className='sm:flex text-slate-700 hover:bg-slate-300 p-2'><RiLoginBoxLine /></li>
+                    <Link to='/profile'>
+                        {currentUser ? (
+                            <img
+                                className='rounded-full h-7 w-7 object-cover'
+                                src={currentUser.avatar}
+                                alt="Profile"
+                            />
+                        ) : (
+                            <li className='hidden sm:inline text-slate-700 hover:underline'>Signin</li>
+                        )
+                        }
                     </Link>
+                    {/* <Link to='/signin'>
+                        <li className='sm:flex text-slate-700 hover:bg-slate-300 p-2'><RiLoginBoxLine /></li>
+                    </Link> */}
                 </ul>
             </div>
         </header>
