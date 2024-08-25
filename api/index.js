ba6cb3af10e468a8a8b5d6eeca1e3bae3d496165
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-08-25 20:21:31 +0530

    User Update API Route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
+import cookieParser from "cookie-parser";
+import updateRoute from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Connected to MongoDB');
@@ -18,9 +19,11 @@ app.listen(3000, () => {
 })
 
 app.use(express.json()); // it will allow to take json data as input
+app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
 
 //middleware
 app.use((err, req, res, next) => {
