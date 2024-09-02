3765fec64abad196401ed104ecfaeb9016093a8e
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-09-02 20:15:56 +0530

    Created delete user-listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -157,20 +157,6 @@ export default function Profile() {
     }
   };
 
-  /* const handleShowListings = async () => {
-    try {
-      setShowListingsError(false)
-      const res = await fetch(`/api/user/listings/${currentUser._id}`);
-      const data = await res.json();
-      if (data.success === false) {
-        setShowListingsError(true)
-        return;
-      }
-      setUserListings(data);
-    } catch (error) {
-      setShowListingsError(true)
-    }
-  } */
   const handleShowListings = () => {
     navigate(`/user/listings/${currentUser._id}`);
   };
@@ -262,35 +248,6 @@ export default function Profile() {
         Show Listings
       </button>
       <p className='text-red-700 text-sm'>{showListingsError ? 'Error displaying listings ⚠️' : ''}</p>
-      {/* {
-        userListings && userListings.length > 0 &&
-        <div className=''>
-          <h1 className='text-center mt-5 text-3xl font-semibold'>Your Listings</h1>
-          {
-            userListings.map((listing) => (
-              <div key={listing._id} className='border rounded-lg p-3 mt-2 flex justify-between items-center gap-3'>
-                <Link to={`/listing/${listing._id}`}>
-                  <img src={listing.imageUrl[0]} alt='listing cover img'
-                    className='h-16 w-16 object-contain'
-                  />
-                </Link>
-                <Link
-                  to={`/listing/${listing._id}`}
-                  className='text-slate-700 font-semibold flex-1 hover:underline truncate'
-                >
-                  <p>
-                    {listing.name}
-                  </p>
-                </Link>
-                <div className='flex flex-col items-center'>
-                  <button className='text-red-700 uppercase'>Delete</button>
-                  <button className='text-green-700 uppercase'>Edit</button>
-                </div>
-              </div>
-            ))
-          }
-        </div>
-      } */}
     </div>
   );
 }
--- client/src/pages/UserListings.jsx
+++ client/src/pages/UserListings.jsx
@@ -33,9 +33,27 @@ const UserListings = () => {
         fetchUserListings();
     }, [userId]);
 
+    const handleDeleteListing = async (listingId) => {
+        try {
+            const res = await fetch(`/api/listing/delete/${listingId}`,
+                {
+                    method: 'DELETE',
+                },
+            )
+            const data = await res.json();
+            if (data.success === false) {
+                console.log(data.message);
+                return;
+            }
+            setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
     return (
         <div className='p-3 max-w-lg mx-auto'>
-            <Link to={'/profile'}>
+            <Link to={'/profile'} className='backIconContainer'>
                 <div className='backIconStyle'>
                     <p><IoReturnDownBack /></p>
                 </div>
@@ -62,7 +80,7 @@ const UserListings = () => {
                                     </p>
                                 </Link>
                                 <div className='flex flex-col items-center'>
-                                    <button className='text-red-700 uppercase'>Delete</button>
+                                    <button onClick={() => handleDeleteListing(listing._id)} className='text-red-700 uppercase'>Delete</button>
                                     <button className='text-green-700 uppercase'>Edit</button>
                                 </div>
                             </div>
