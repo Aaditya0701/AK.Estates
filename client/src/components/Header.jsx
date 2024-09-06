f6c0619cf0cf340c36818ad7986761a47f9a4951
Aaditya Kamlaskar <aadityakamlaskar1211@gmail.com>
2024-09-07 01:06:32 +0530

    Create Search API route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,6 +29,9 @@ export default function Header() {
         }
     };
 
+    const isAuthPage = location.pathname === '/signin' || location.pathname === '/signup';
+
+
     return (
         <header className='bg-slate-200 shadow-md headerStyle'>
             <div className='flex justify-between items-center max-w-7xl mx-auto p-3'>
@@ -38,10 +41,12 @@ export default function Header() {
                         <spam className='text-slate-700'>Estate</spam>
                     </h1>
                 </Link>
-                <form className='bg-slate-100 p-2 rounded-3xl flex items-center'>
-                    <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-                    <FaSearch className='text-slate-600' />
-                </form>
+                {!isAuthPage && (
+                    <form className='bg-slate-100 p-2 rounded-3xl flex items-center'>
+                        <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+                        <FaSearch className='text-slate-600' />
+                    </form>
+                )}
                 <ul className='flex items-center gap-5'>
                     {location.pathname.includes('/listing') && (
                         <Link to={`/user/listings/${currentUser?._id}`} className='backIconContainer'>
